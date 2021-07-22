882c13f424263192c9900fe1056ddaa2a3b52c7f
kumarmayank48066 <86884032+kumarmayank48066@users.noreply.github.com>
2021-07-22 10:13:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,98 +1,131 @@
-var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
- 
-var particles = [];
-var plinkos = [];
-var divisions =[];
-var divisionHeight=300;
-var score =0;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,polygon,ground;
+var stand1,stand2;
+var polygon;
+var slingShot;
+var polygon_img;
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(900,400);
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(width/2,height,width,20);
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+  //level one
+  block1 = new Block(300,275,30,40);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  //level three
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //top
+  block16 = new Block(390,155,30,40);
 
-  //create division objects
-  for (var k = 0; k <=80; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=160; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=240; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=320; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=400; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=480; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=560; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=640; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=720; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-  for (var k = 0; k <=800; k = k + 80) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
+  //set 2 for second stand
+  //level one
+  blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40);
+  blocks3 = new Block(700,175,30,40);
+  blocks4 = new Block(730,175,30,40);
+  blocks5 = new Block(760,175,30,40);
+  //level two
+  blocks6 = new Block(670,135,30,40);
+  blocks7 = new Block(700,135,30,40);
+  blocks8 = new Block(730,135,30,40);
+  //top
+  blocks9 = new Block(700,95,30,40);
+
+  //polygon holder with slings
+  polygon = Bodies.circle(50,200,20);
+  World.add(world,polygon);
   
-  //create 1st row of plinko objects
-  for (var j = 75; j <=width; j=j+50) { 
-    plinkos.push(new Plinko(j,75));
-  }
+  slingShot = new Slingshot(this.polygon,{x:100,y:200});
 
-  //create 2nd row of plinko objects
-  for (var j = 50; j <=width-10; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,175));
-  }
-
-  //create 3rd row of plinko objects
-  for (var j = 25; j <=width-10; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,275));
-  }
+}
+function draw() {
+  background(56,44,44); 
+ 
+  //Engine.update(engine);
   
-  //create 4th row of plinko objects
-  for (var j = 0; j <=width-10; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,375));
-  }
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the polygon to destroy the blocks",300,30);
+  textSize(10);
+  text("Press Space to get a second Chance to Play!!",650 ,350);
+  ground.display();
+  stand1.display();
+  stand2.display();
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+  fill("skyblue");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("turquoise");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("pink")
+  blocks9.display();
+  fill("gold");
+  imageMode(CENTER)
+  image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
 
-  //create particle objects
-  
+  slingShot.display();
   
 }
- 
-
-
-function draw() {
-  background("black");
-  textSize(20)
- 
-  Engine.update(engine);
-  ground.display();
-  
-  //display the plinkos 
-  for (var i = 0; i < plinkos.length; i++) {
-    plinkos[i].display();   
-  }
-   
-  //display the divisions
-  for (var k = 0; k < divisions.length; k++) {
-    divisions[k].display();
-  }
-
-  //display the paricles 
-
+function mouseDragged(){
+  Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY});
+}slingshot.attach(polygonObj.body);
+function mouseReleased(){
+  slingShot.fly();
+}
+function keyPressed(){
+//if (keyCode === 32) {
+  if (keyCode === 32) {
+    Matter.Body.setPosition(polygonObj.body, {x:235, y:420}) 
+  }slingshot.attach(polygonObj.body);
 }
